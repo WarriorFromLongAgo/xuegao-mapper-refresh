acd811d9ade42478e6630ba2fc7a06f15e26976d
xuegao <hubeixuegao@gmail.com>
2022-08-05 09:39:29 +0800

    mybatis

--- src/main/java/com/xuegao/xuegaomapperrefresh/refresh/MapperRefreshBean.java
+++ src/main/java/com/xuegao/xuegaomapperrefresh/refresh/MapperRefreshBean.java
@@ -1,13 +1,5 @@
 package com.xuegao.xuegaomapperrefresh.refresh;
 
-import com.xuegao.framework.mybatis.CommonXMLMapperBuilder;
-import com.xuegao.framework.mybatis.KeyGenMode;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
-import java.util.Properties;
-import javax.sql.DataSource;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -21,9 +13,14 @@ import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.util.CollectionUtils;
+
+import javax.sql.DataSource;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
 
 public class MapperRefreshBean {
     private ApplicationContext context;
@@ -42,10 +39,10 @@ public class MapperRefreshBean {
     }
 
     private void addBean(String beanName, Class beanClass, Object... constructValues) {
-        DefaultListableBeanFactory beanDefReg = (DefaultListableBeanFactory)((ConfigurableApplicationContext)this.context).getBeanFactory();
+        DefaultListableBeanFactory beanDefReg = (DefaultListableBeanFactory) ((ConfigurableApplicationContext) this.context).getBeanFactory();
         BeanDefinitionBuilder beanDefBuilder = BeanDefinitionBuilder.genericBeanDefinition(beanClass);
         if (constructValues != null && constructValues.length > 0) {
-            for(int i = 0; i < constructValues.length; ++i) {
+            for (int i = 0; i < constructValues.length; ++i) {
                 beanDefBuilder.addConstructorArgValue(constructValues[i]);
             }
         }
@@ -55,20 +52,20 @@ public class MapperRefreshBean {
     }
 
     private void removeBean(String beanName) {
-        DefaultListableBeanFactory beanDefReg = (DefaultListableBeanFactory)((ConfigurableApplicationContext)this.context).getBeanFactory();
+        DefaultListableBeanFactory beanDefReg = (DefaultListableBeanFactory) ((ConfigurableApplicationContext) this.context).getBeanFactory();
         beanDefReg.getBeanDefinition(beanName);
         beanDefReg.removeBeanDefinition(beanName);
     }
 
     public void refreshSqlSessionFacotry() throws Exception {
-        SqlSessionFactory oldSqlSessionFactory = (SqlSessionFactory)this.context.getBean("sqlSessionFactory");
+        SqlSessionFactory oldSqlSessionFactory = (SqlSessionFactory) this.context.getBean("sqlSessionFactory");
         Configuration oldConfiguration = oldSqlSessionFactory.getConfiguration();
-        CommonXMLMapperBuilder builder = new CommonXMLMapperBuilder();
-        builder.setBaseResultMap("BaseResultMap");
-        builder.setBaseTableName("BaseTable");
-        builder.setGenerationType("GenerationType");
-        builder.setBaseColumns("BaseColumns");
-        builder.setKeyGenMode(KeyGenMode.parse(StringUtils.defaultString(this.keyGenMode, KeyGenMode.IDENTITY.getCode())));
+        // CommonXMLMapperBuilder builder = new CommonXMLMapperBuilder();
+        // builder.setBaseResultMap("BaseResultMap");
+        // builder.setBaseTableName("BaseTable");
+        // builder.setGenerationType("GenerationType");
+        // builder.setBaseColumns("BaseColumns");
+        // builder.setKeyGenMode(KeyGenMode.parse(StringUtils.defaultString(this.keyGenMode, KeyGenMode.IDENTITY.getCode())));
         SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
         factory.setDataSource(this.dataSource);
         if (this.properties.getConfigLocation() != null) {
@@ -85,20 +82,20 @@ public class MapperRefreshBean {
 
         factory.setTypeAliasesPackage(this.properties.getTypeAliasesPackage());
         factory.setTypeHandlersPackage(this.properties.getTypeHandlersPackage());
-        Resource[] mapperLocations = builder.builderCommonMapper(this.properties.resolveMapperLocations());
-        factory.setMapperLocations(mapperLocations);
+        // Resource[] mapperLocations = builder.builderCommonMapper(this.properties.resolveMapperLocations());
+        // factory.setMapperLocations(mapperLocations);
         SqlSessionFactory sqlSessionFactory = factory.getObject();
         List<Interceptor> interceptors = oldConfiguration.getInterceptors();
         if (!CollectionUtils.isEmpty(interceptors)) {
             Iterator var9 = interceptors.iterator();
 
-            while(var9.hasNext()) {
-                Interceptor interceptor = (Interceptor)var9.next();
+            while (var9.hasNext()) {
+                Interceptor interceptor = (Interceptor) var9.next();
                 sqlSessionFactory.getConfiguration().addInterceptor(interceptor);
             }
         }
 
-        SqlSessionFactory sqlSessionFactory = new DefaultSqlSessionFactory(sqlSessionFactory.getConfiguration());
+        sqlSessionFactory = new DefaultSqlSessionFactory(sqlSessionFactory.getConfiguration());
         this.sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory);
     }
 
